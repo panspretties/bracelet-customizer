2358d76972d3c1477bb849a211353c963382bd71
panspretties <xdempseyx@hotmail.com>
2019-08-28 16:36:51 -0700

    add bead

--- scripts/beads.js
+++ scripts/beads.js
@@ -29,4 +29,9 @@ let jsonObject = [
 		"type" : "metal",
 		"path" : "Attachment_1567022285.jpeg"
 	},
+	{
+		"name" : "GOT Bridge",
+		"type" : "metal",
+		"path" : "Attachment_1567022291.jpeg"
+	},
 ]
